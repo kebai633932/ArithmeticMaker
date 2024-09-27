1f914aa7d056be9c5591e90b44777d2bc32891c0
kebai633932 <3340201045@qq.com>
2024-09-27 11:51:22 +0800

    第二次提交，修改一些warning

--- src/main/java/com/kjh/model/CreateFraction.java
+++ src/main/java/com/kjh/model/CreateFraction.java
@@ -59,7 +59,7 @@ public class CreateFraction {
             s.append(currentOperator).append(num); // 拼接表达式
         }
 
-        int greatFactor = greatFactor(x, y); // 计算最大公因数
+        int greatFactor = greatFactor_GCD(x, y); // 计算最大公因数
         x /= greatFactor; // 结果化简
         y /= greatFactor;
 
@@ -69,60 +69,79 @@ public class CreateFraction {
         return new String[]{s.toString(), res}; // 返回表达式和结果
     }
 
+
+
     /**
-     * 求最大公因数
+     * 用辗转相除法求最大公因数
      * @param x 第一个数
      * @param y 第二个数
      * @return 最大公因数
      */
-    public int greatFactor(int x, int y) {
-        while (true) {
-            if (x % y == 0) {
-                return y; // 如果能整除，返回y
-            }
-            int temp = y; // 进行欧几里得算法
+    public int greatFactor_GCD(int x, int y) {
+        if (x == 0||y == 0)
+            System.out.println("输入数据错误，辗转相除法中，最开始的除数和被除数都不能为零");
+        while (y != 0) {
+            int temp = y;
             y = x % y;
             x = temp;
         }
+        return x; // 当y为0时，x就是最大公因数
     }
 
     /**
      * 生成一对互质数
-     * @param range 范围，用于生成随机数
+     * @param range 随机数范围
      * @param random 随机数生成器
      * @return 互质数数组
      */
     public int[] createCoprimeNumbers(int range, Random random) {
-        int x = 1 + random.nextInt(range); // 生成随机数
-        int y = 1 + random.nextInt(range);
-        int greatFactor = greatFactor(x, y); // 计算最大公因数
-        x /= greatFactor; // 化简
-        y /= greatFactor;
-        return new int[]{x, y}; // 返回互质数
+        int x, y;
+        // 循环直到找到一对互质数
+        do {
+            x = 1 + random.nextInt(range); // 生成随机数 x
+            y = 1 + random.nextInt(range); // 生成随机数 y
+        } while (greatFactor_GCD(x, y) != 1); // 检查 x 和 y 是否互质
+        return new int[]{x, y}; // 返回互质数对
     }
 
+
     /**
-     * 假分数转化为真分数
+     * 将假分数转换为真分数
      * @param x 分子
      * @param y 分母
      * @return 真分数字符串
      */
     public String shamToProperFraction(int x, int y) {
-        if (x > y) { // 如果假分数
-            int n = x / y; // 整数部分
-            x = (x - n * y); // 计算新的分子
+        // 判断是否为假分数
+        if (x > y) {
+            int n = x / y; // 计算整数部分
+            x = x % y; // 计算新的分子
+
+            // 如果分子为0，返回整数部分
             if (x == 0) {
-                return String.valueOf(n); // 如果分子为0，返回整数部分
+                System.out.println("输入分母数据错误，小于0");
+                return String.valueOf(n);
             }
-            return n + "'" + x + "/" + y; // 返回带分数形式
-        } else if (x == y) {
-            return "1"; // 如果分子和分母相等
-        } else if (y == 1) {
-            return String.valueOf(x); // 如果分母为1
-        } else if (x == 0) {
-            return "0"; // 如果分子为0
+
+            // 返回带分数形式，例如 "1'2/3"
+            return n + "'" + x + "/" + y;
         }
-        return x + "/" + y; // 返回正常分数形式
+        // 如果分子和分母相等，返回1
+        else if (x == y) {
+            return "1";
+        }
+        // 如果分母为1，返回分子
+        else if (y == 1) {
+            return String.valueOf(x);
+        }
+        // 如果分子为0，返回0
+        else if (x == 0) {
+            return "0";
+        }
+
+        // 返回正常分数形式，例如 "2/3"
+        return x + "/" + y;
     }
+
 }
 
